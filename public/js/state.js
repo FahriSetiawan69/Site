ba22342daace7e129320a90b77b206e803694ade
FahriSetiawan69 <94859061+FahriSetiawan69@users.noreply.github.com>
2026-01-07 03:45:19 +0700

    Update state.js

--- public/js/state.js
+++ public/js/state.js
@@ -1,11 +1,9 @@
-window.AuthState = {
-  isLoggedIn() {
-    return sessionStorage.getItem("fishit_key_valid") === "true";
-  },
-  login() {
-    sessionStorage.setItem("fishit_key_valid", "true");
-  },
-  logout() {
-    sessionStorage.removeItem("fishit_key_valid");
-  }
-};
+let activeAccountId = null;
+
+function setActiveAccount(id) {
+  activeAccountId = id;
+}
+
+function getActiveAccount() {
+  return dummyAccounts.find(acc => acc.id === activeAccountId);
+}

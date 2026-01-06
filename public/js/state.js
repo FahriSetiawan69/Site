831fca06e7a17b12eed26cf033e51e00d81a1596
FahriSetiawan69 <94859061+FahriSetiawan69@users.noreply.github.com>
2026-01-06 20:00:10 +0700

    Update state.js

--- public/js/state.js
+++ public/js/state.js
@@ -4,11 +4,9 @@ window.AuthState = {
   isLoggedIn() {
     return sessionStorage.getItem("fishit_key_valid") === "true";
   },
-
   login() {
     sessionStorage.setItem("fishit_key_valid", "true");
   },
-
   logout() {
     sessionStorage.removeItem("fishit_key_valid");
   }

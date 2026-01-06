18f810a7685aa5bc2d383d9a81fdeb321881e1bb
FahriSetiawan69 <94859061+FahriSetiawan69@users.noreply.github.com>
2026-01-06 20:00:47 +0700

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,26 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const input = document.getElementById("keyInput");
   const status = document.getElementById("status");
 
-  if (!btn || !input) {
-    console.error("Login elements not found");
-    return;
-  }
-
   btn.addEventListener("click", () => {
     const key = input.value.trim();
-
     console.log("Login attempt:", key);
 
     if (key === "abogoboga") {
       AuthState.login();
-
       status.innerText = "Key validated successfully.";
-      status.style.color = "green";
-
+      status.style.color = "lime";
       setTimeout(() => {
         window.location.href = "dashboard.html";
       }, 500);
-
     } else {
       status.innerText = "Invalid key.";
       status.style.color = "red";

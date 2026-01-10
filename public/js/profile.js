f476de652bcfc7ab1b6211bc6d44ebd09a8b4f8c
FahriSetiawan69 <94859061+FahriSetiawan69@users.noreply.github.com>
2026-01-11 01:53:29 +0700

    Create profile.js

---
+++ public/js/profile.js
@@ -0,0 +1,7 @@
+document.getElementById("profileView").innerHTML = `
+  <h2>User Profile</h2>
+  <p><b>Username:</b> Fahri</p>
+  <p><b>Role:</b> Developer</p>
+  <p><b>Active Key:</b> abogoboga</p>
+  <p><b>Status:</b> Premium</p>
+`;
